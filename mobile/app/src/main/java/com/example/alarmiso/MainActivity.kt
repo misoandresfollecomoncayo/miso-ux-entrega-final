128752d5107176f7212e7fef7aaba5705a719f10
privera <p.riverah@uniandes.edu.co>
2025-09-21 19:35:26 -0500

    Se añade la pantalla de autenticación y otras configuraciones

--- mobile/app/src/main/java/com/example/alarmiso/MainActivity.kt
+++ mobile/app/src/main/java/com/example/alarmiso/MainActivity.kt
@@ -1,47 +1,30 @@
 package com.example.alarmiso
 
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.alarmiso.ui.theme.AlarMISOTheme
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.alarmiso.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class MainActivity : ComponentActivity() {
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            AlarMISOTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
-    }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AlarMISOTheme {
-        Greeting("Android")
+        val navController = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment)!!
+            .findNavController()
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        bottomNavigationView.setupWithNavController(navController)
+
+
     }
 }
---
+++ mobile/app/src/main/java/com/example/alarmiso/ui/auth/AuthFragment.kt
@@ -0,0 +1,14 @@
+package com.example.alarmiso.ui.auth
+
+import android.widget.Adapter
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.RecyclerView
+import com.example.alarmiso.databinding.Fragment
+class AuthFragment: Fragment() {
+
+    private lateinit var recyclerView: RecyclerView
+
+    private var _binding: FragmentPerfor
+
+}
